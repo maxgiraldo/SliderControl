adbadbb4040a36427ea0d514ff3e2b0fd1161d64
Max Giraldo <maxgiraldo@users.noreply.github.com>
2017-01-14 09:21:50 -0500

    Remove Pod for now

--- Example/Pods/Target Support Files/SliderControl/SliderControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SliderControl : NSObject
-@end
-@implementation PodsDummy_SliderControl
-@end
--- Example/Pods/Target Support Files/SliderControl/SliderControl-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SliderControlVersionNumber;
-FOUNDATION_EXPORT const unsigned char SliderControlVersionString[];
-
